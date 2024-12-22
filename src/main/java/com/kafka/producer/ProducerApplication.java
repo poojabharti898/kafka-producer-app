a986d679daca570d3909d2129c274a2e54be0fab
Pooja <poojabharti898@gmail.com>
2024-12-22 18:26:47 +0530

    Testing git

--- src/main/java/com/kafka/producer/ProducerApplication.java
+++ src/main/java/com/kafka/producer/ProducerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProducerApplication {
-
+//comment added to check if git is working
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
 	}
